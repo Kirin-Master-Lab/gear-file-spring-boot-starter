edb47613d0b1bbb198f3f8de02be5c4e8ae5c113
chenLiang <chenliang@abupdate.com>
2026-02-03 09:55:49 +0800

    feat: 优化

--- src/main/java/com/gear/file/config/GearFileAutoConfiguration.java
+++ src/main/java/com/gear/file/config/GearFileAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.gear.file.config;
 
 import com.gear.file.core.FileEngine;
 import com.gear.file.strategy.FileParser;
+import com.gear.file.strategy.impl.ExcelFileParser;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -11,8 +12,14 @@ import org.springframework.core.io.ResourceLoader;
 import java.util.List;
 
 @AutoConfiguration
-@ComponentScan(basePackages = "com.gear.file")
 public class GearFileAutoConfiguration {
+
+
+    @Bean
+    public ExcelFileParser excelFileParser() {
+        return new ExcelFileParser();
+    }
+
     @Bean
     @ConditionalOnMissingBean
     public FileEngine fileEngine(ResourceLoader resourceLoader, List<FileParser> parsers) {
